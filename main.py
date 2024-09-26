321250a45e23affbe56aea28f64c3301045d548b
Александр <Symenkovsasha@gmail.com>
2024-09-26 22:49:40 +0700

    Практическое задание по уроку "Базовые структуры данных"

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
-# This is a sample Python script.
+# 1st program
+print(9 ** 0.5 * 5)
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# 2nd program
+print(9.99 > 9.98 and 1000 != 1000.1)
 
+# 3rd program
+print(2 * 2 + 2 == 2 * (2 + 2))
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# 4th program
+a = float("123.456") * 10
+intA = int(a)
+noA = ((intA // 10) * 10)
+print(intA - noA)
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
