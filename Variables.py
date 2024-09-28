96c294be1dfc59448c046b8fbe31598649dddfa4
Александр <Symenkovsasha@gmail.com>
2024-09-28 20:42:56 +0700

    Практическое задание по уроку "Базовые структуры данных"

---
+++ Variables.py
@@ -0,0 +1,5 @@
+homework = 12
+hours_spent = 1.5
+course_name = 'Python,'
+time_for_one_task = (hours_spent / homework)
+print('Курс:',course_name,'всего задач:', homework, 'затрачено часов:', hours_spent, 'среднее время выполнения', time_for_one_task )
