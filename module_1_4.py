0376c7597a9d756ee5328b6277976a6d5a06e314
Александр <Symenkovsasha@gmail.com>
2024-09-29 21:08:41 +0700

    ...

---
+++ module_1_4.py
@@ -0,0 +1,12 @@
+my_string = input('Напишите свое имя: ')
+print(my_string .upper())
+my_string2 = input('Напишите вашу фамилию: ')
+print(my_string2 .upper() .lower())
+name = my_string
+surname = my_string2
+print('Приветствую вас,',name ,surname, ',мне нужно будет задать еще пару вопрос' .replace(' ', ''))
+my_string3 = "Cегодня очень хорошая погода"
+print(my_string3[0])
+print(my_string3[-1])
+
+
