ca56af0b30e2a88282778f46e07d2ed3ba2d8790
Александр <Symenkovsasha@gmail.com>
2024-10-15 19:33:18 +0700

    ///

---
+++ module_3_2.py
@@ -0,0 +1,19 @@
+def send_email(message, recipient, sender="university.help@gmail.com"):
+    if ("@" and (".com" or ".ru" or ".net")) not in (recipient or sender) or (
+            "@" or (".com" or "ru" or ".net")) not in (recipient or sender):
+        print(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}")
+    elif recipient == sender:
+        print("Нельзя отправить письмо самому себе!")
+    elif recipient == "university.help@gmail.com":
+        print(f"Письмо успешно отправлено с адреса {sender} на адрес {recipient}.")
+    else:
+        print(f"НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}.")
+
+
+send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
+
+send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
+
+send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
+
+send_email('Напоминаю себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
