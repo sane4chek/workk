313687999e9ad533c1cff96313ef6e44c0dd92f7
Александр <Symenkovsasha@gmail.com>
2024-10-27 16:04:07 +0700

    ///

---
+++ module_4_1.py
@@ -0,0 +1,15 @@
+from true_math import divide as div_1
+from fake_math import divide as div_2
+
+result1 = div_1(69, 3)
+
+result2 = div_1(3, 0)
+
+result3 = div_2(49, 7)
+
+result4 = div_2(15, 0)
+
+print(result1)
+print(result2)
+print(result3)
+print(result4)
---
+++ true_math.py
@@ -0,0 +1,9 @@
+from math import inf
+
+def divide(first, second):
+    if second != 0:
+        rep = first / second
+        return rep
+    else:
+        inf
+
