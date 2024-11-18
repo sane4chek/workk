76bc5041bc9cbbf4343f27b0eb32f628c4495bee
Александр <Symenkovsasha@gmail.com>
2024-11-18 20:42:01 +0700

    ///

--- module_4_2.py
+++ module_4_2.py
@@ -1,12 +1,8 @@
-
-
-
 def test_function():
     def inner_function():
         print(f"Я в области видимости функции test_function")
-        inner_function()
+
+    inner_function()
+
 
 test_function()
-
-
-
