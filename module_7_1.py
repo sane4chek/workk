8d38e299746c5950c78bc696c0ecd774f2d39083
Александр <Symenkovsasha@gmail.com>
2024-11-29 19:36:01 +0700

    ///

--- module_7_1.py
+++ module_7_1.py
@@ -32,15 +32,14 @@ class Shop:
                     file.write(str(product) + '\n')
 
 
-# Пример работы программы
+
 s1 = Shop()
 
 p1 = Product('Potato', 50.5, 'Vegetables')
 p2 = Product('Spaghetti', 3.4, 'Groceries')
 p3 = Product('Potato', 5.5, 'Vegetables')
 
-print(p2)  # str
-
+print(p2)
 s1.add(p1, p2, p3)
 
 print(s1.get_products())
